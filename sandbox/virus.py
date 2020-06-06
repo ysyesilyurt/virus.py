0efd07b52d6c0b7c8ed4c598646bc07b78150766
ysyesilyurt <yesilyurt.selim@metu.edu.tr>
2020-06-06 18:06:11 +0300

    initialized

---
+++ sandbox/virus.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+
+def execute_payload():
+    pass
+
+
+def create_copy():
+    pass
+
+
+def otp_keygen(ln):
+    pass
+
+
+def encrpyt_it(stuff_to_encrpyt):
+    pass
+
+
+def wrap(asset):
+    pass
+
+
+def infect(troy):
+    pass
+
+
+def main():
+    execute_payload()
+    asset = create_copy()
+    troy = wrap(asset)
+    infect(troy)
+
+
+if __name__ == '__main__':
+    main()
